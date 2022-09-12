a5e306b9d63a9580d662451649c33e3d81d1b6de
mikevhe18 <viriyananda.michael@gmail.com>
2022-09-12 10:10:02 +0700

    [14.0.1.0.0] Without Transaction Open & No Master

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,28 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "{description_module}",
-    "version": "11.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "{category_module}",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "{dependencies_module}"
+        "{dependencies_module}",
     ],
     "data": [
+        "security/ir.model.access.csv",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "{view/wizard/template}",
     ],
     "demo": [],
     "images": [
---
+++ models/{model_name}.py
@@ -0,0 +1,89 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class {model_name:CamelCase}(models.Model):
+    _name = "{model_name}"
+    _description = "{model_description}"
+    _inherit = [
+        "mixin.transaction_confirm",
+        "mixin.transaction_done",
+        "mixin.transaction_cancel",
+        "mixin.date_duration",
+    ]
+
+    # Multiple Approval Attribute
+    _approval_from_state = "draft"
+    _approval_to_state = "done"
+    _approval_state = "confirm"
+    _after_approved_method = "action_done"
+
+    # Attributes related to add element on view automatically
+    _automatically_insert_view_element = True
+    _automatically_insert_done_button = False
+    _automatically_insert_done_policy_fields = False
+
+    # Attributes related to add element on form view automatically
+    _automatically_insert_multiple_approval_page = True
+    _statusbar_visible_label = "draft,confirm,done,reject"
+    _policy_field_order = [
+        "confirm_ok",
+        "approve_ok",
+        "reject_ok",
+        "restart_approval_ok",
+        "cancel_ok",
+        "restart_ok",
+        "manual_number_ok",
+    ]
+    _header_button_order = [
+        "action_confirm",
+        "action_approve_approval",
+        "action_reject_approval",
+        "%(ssi_transaction_cancel_mixin.base_select_cancel_reason_action)d",
+        "action_restart",
+    ]
+
+    # Attributes related to add element on search view automatically
+    _state_filter_order = [
+        "dom_draft",
+        "dom_confirm",
+        "dom_reject",
+        "dom_done",
+        "dom_cancel",
+    ]
+
+    # Sequence attribute
+    _auto_fill_sequence = True
+    _create_sequence_state = "done"
+
+    @api.model
+    def _get_policy_field(self):
+        res = super({model_name:CamelCase}, self)._get_policy_field()
+        policy_field = [
+            "confirm_ok",
+            "approve_ok",
+            "done_ok",
+            "cancel_ok",
+            "reject_ok",
+            "restart_ok",
+            "restart_approval_ok",
+            "manual_number_ok",
+        ]
+        res += policy_field
+        return res
+
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("draft", "Draft"),
+            ("confirm", "Waiting for Approval"),
+            ("done", "Done"),
+            ("cancel", "Cancelled"),
+            ("reject", "Rejected"),
+        ],
+        default="draft",
+        copy=False,
+    )
